d0353108eb7f3d3acf0c8dc2093dc8666446941b
daniel martinez <martinda5@students.pittschools.org>
2018-02-17 09:33:17 -0500

    Changed DeployHookCommand

--- src/org/usfirst/frc/team2642/robot/commands/climb/DeployHookCommand.java
+++ src/org/usfirst/frc/team2642/robot/commands/climb/DeployHookCommand.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team2642.robot.commands.climb;
 
+import org.usfirst.frc.team2642.robot.OI;
+import org.usfirst.frc.team2642.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -8,8 +11,7 @@ import edu.wpi.first.wpilibj.command.Command;
 public class DeployHookCommand extends Command {
 
     public DeployHookCommand() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+    	requires(Robot.climb);
     }
 
     // Called just before this Command runs the first time
@@ -18,6 +20,12 @@ public class DeployHookCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	if (isAButtonPressed()) {
+    		Robot.climb.deployHook();
+    	}
+    	else if(isXButtonPressed()) {
+    		Robot.climb.resetHook();
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -32,5 +40,21 @@ public class DeployHookCommand extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    }
+    
+    public boolean isAButtonPressed() {
+    	if(OI.auxXbox.getAButtonPressed()) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+    
+    public boolean isXButtonPressed() {
+    	if(OI.auxXbox.getXButtonPressed()) {
+    	return true;
+    } else {
+    	return false;	
+    	}
     }
 }
