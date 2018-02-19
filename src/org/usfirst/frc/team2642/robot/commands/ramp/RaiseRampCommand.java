d6851e79db5b1a43ab6745c98848d12c5cfc7617
dylan thorne <river.djt@gmail.com>
2018-02-19 17:03:05 -0500

    changed RaiseRamp cylinder to Intake cylinder part 2

--- src/org/usfirst/frc/team2642/robot/commands/ramp/RaiseRampCommand.java
+++ src/org/usfirst/frc/team2642/robot/commands/ramp/RaiseRampCommand.java
@@ -25,12 +25,12 @@ public class RaiseRampCommand extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	if (Robot.ramp.raiseRampCylinder.get() == Value.kForward) {
+    	/*if (Robot.ramp.raiseRampCylinder.get() == Value.kForward) {
             return true;
     	}
-    	else {
+    	else { */
             return false;
-    	}
+    	//}
     }
 
     // Called once after isFinished returns true
